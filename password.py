df38aad88227047dd45d8815c7f07a2461520fb6
Hyacinthe04 <hyacinthen04@gmail.com>
2019-02-11 11:30:37 +0200

    create class

---
+++ password.py
@@ -0,0 +1,14 @@
+class User:
+    """
+    Class that generates new instances of contacts.
+    """
+
+    user_list = [] # Empty user list
+
+    def __init__(self,username,password):
+
+      # docstring removed for simplicity
+
+        self.username = username
+        self.password = password
+        
